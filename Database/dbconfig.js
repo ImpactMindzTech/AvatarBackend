be4f902a2d27e829234c135dd9233ef1536e03c5
sanjayimpact <sanjay.impactmindz@gmail.com>
2024-12-19 18:44:33 +0530

    update

--- Database/dbconfig.js
+++ Database/dbconfig.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 export const connect =async()=>{
 try{
-    await  mongoose.connect(`${process.env.MongoUrl}`)
-    // await  mongoose.connect("mongodb://localhost:27017/sanjutest");
+    //await  mongoose.connect(`${process.env.MongoUrl}`)
+     await  mongoose.connect("mongodb+srv://avatar:avatar@mycluster.vq0yt.mongodb.net/AvatarWalk");
     console.log("successfully connected");
 
 }
